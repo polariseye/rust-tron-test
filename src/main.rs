f4a4dca8eae1f96e6b575de4c6b2d7a051733554
liujun <polariseye@126.com>
2022-07-29 17:48:33 +0800

    commit all code

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ use hex::FromHex;
 use once_cell::sync::Lazy;
 use tonic::transport::Channel;
 use tracing_subscriber::FmtSubscriber;
-use crate::tron_grpc::{Account, EmptyMessage};
+use crate::tron_grpc::{Account, AccountBalanceRequest, AccountIdentifier, EmptyMessage};
 use crate::tron_grpc::wallet_client::WalletClient;
 use prost::Message;
+use tonic::IntoRequest;
+use crate::tron_grpc::transaction::contract::ContractType;
 
 const PRIVATE_KEY_1:&str="4830EC8DBE1E91557F6AE6A2EFED706821EEA5AD39FEC431B8C6860F486EF572";
 const PUBLIC_KEY_1:&str="04779D7BD7B5A316DE28C30FB5B12E215718AA423F42B6C1CDA8EF61E845F2978A58529458D6D4B31FB2328DB62CBB341FC0815172F79C3423C42B7D5A5F1888E2";
@@ -43,8 +45,7 @@ async fn main() {
 
     tracing::subscriber::set_global_default(subscriber)
         .expect("setting default subscriber failed");
-
-    get_trc20_balance().await;
+    get_trc20_fee_test().await;
 }
 
 async fn get_client()->WalletClient<Channel>{
@@ -54,7 +55,7 @@ async fn get_client()->WalletClient<Channel>{
 async fn get_balance_tst(){
     let mut client =  get_client().await;
 
-    let addr="41f4f917ee28322a788a8e712fa6bbcf4f8e149f34";
+    let addr="41f4f917ee28322a788a8e712fa6bbcf4f8e149f33";
     let request_obj=Account{
         account_name: vec![],
         r#type: 0,
@@ -94,6 +95,9 @@ async fn get_balance_tst(){
         witness_permission: None,
         active_permission: vec![]
     };
+    let mut request_obj:Account=Default::default();
+    request_obj.address= hex::decode(addr).expect("decode addr error");
+
     let resp= client.get_account(request_obj).await.expect("get account error");
 
     let account= resp.into_inner();
@@ -162,6 +166,7 @@ async fn call_trc20<TInput:Tokenize,TOutput:Detokenize>(client_obj:&mut WalletCl
     println!("result:{:?}",&result);
 
     let result =result.into_inner();
+    println!("energy_used:{}",result.energy_used);
     ethers::contract::decode_function_data::<TOutput,_>(fn_obj,&result.constant_result[0],false).expect("decode error")
 }
 
@@ -316,7 +321,7 @@ async fn transfer_trc10(){
 async fn transfer_trc20(){
     let mut client =  get_client().await;
     let from=hex::decode( ADDR_HEX_1).expect("decode error");
-    let to=hex::decode( ADDR_HEX_2).expect("decode error");
+    let to=hex::decode( "411F4DC709965643A75F330E554A24023AA0203CC6").expect("decode error");
     let amount:ethers::types::U256 =U256::from( 5000000u128); // 2个
     let secret_obj=private::Private::from_hex (PRIVATE_KEY_1).expect("decode error");
     let mut contract_addr_bytes=   "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj".from_base58().expect("base 58 decode error");
@@ -371,7 +376,7 @@ async fn transfer_trc20(){
         contract: vec![contract],
         scripts: vec![],
         timestamp:now,
-        fee_limit: 0
+        fee_limit: 6000000
     };
 
     //prost::Message::encode()
@@ -391,8 +396,195 @@ async fn transfer_trc20(){
         ret: vec![]
     };
 
+    let mut raw_bytes:Vec<u8>=Vec::new();
+    req.encode(&mut raw_bytes);
+    println!("req len:{}",raw_bytes.len());
+
         let result= client.broadcast_transaction(req).await.expect("broadcast error");
         println!("result: {:?}",&result);
         let result=result.into_inner();
         println!("message:{}",   String::from_utf8(result.message).expect("decode message error"));
+}
+
+async fn get_trc20_fee_test(){
+    let mut client =  get_client().await;
+    let from=hex::decode( ADDR_HEX_1).expect("decode error");
+    let to=hex::decode( ADDR_HEX_2).expect("decode error");
+    let amount:ethers::types::U256 =U256::from( 5000000u128); // 2个
+    let secret_obj=private::Private::from_hex (PRIVATE_KEY_1).expect("decode error");
+    let mut contract_addr_bytes=   "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj".from_base58().expect("base 58 decode error");
+    println!("contract addr:{}",hex::encode(&contract_addr_bytes[..contract_addr_bytes.len()-4].to_vec()));
+    //contract_addr_bytes=contract_addr_bytes[1..].to_vec(); // 地址第一个字节是tron前缀，所以需要去掉
+
+    let now_block= client.get_now_block2(EmptyMessage{}).await.expect("get block error");
+    let now_block= now_block.into_inner();
+
+    let fn_obj= ABI.function("transfer").expect("get fn error");
+    let param_data= ethers::contract::encode_function_data(fn_obj,(Address::from_slice(&to[1..]),amount)).expect("encode fn param error");
+    println!("param data:{}",hex::encode( param_data.as_ref()));
+
+    let transfer_tx = tron_grpc::TriggerSmartContract{
+        owner_address: from.clone(),
+        contract_address: contract_addr_bytes[..contract_addr_bytes.len()-4].to_vec(),
+        call_value: 0,
+        data: param_data.to_vec(),
+        call_token_value: 0,
+        token_id: 0
+    };
+    // energy ->通过接口获取
+    // bandwidth -->是一个常量
+    // trx
+    let energy_need= get_trc20_energy_need(&mut client,ADDR_HEX_1,ADDR_HEX_2,amount.as_u128()).await;
+    println!("energy_need:{}",energy_need);
+    let bindwidth_need=get_tx_bandwidth_need(&mut client,transfer_tx,ContractType::TriggerSmartContract).await;
+    println!("bind width need:{}",bindwidth_need);
+
+    let mut request_obj:Account=Default::default();
+    request_obj.address=from.clone();
+    let resp= client.get_account_resource(request_obj).await.expect("get account error");
+    let resp=resp.into_inner();
+    let energy_free= (resp.free_net_limit-resp.free_net_used) +(resp.net_limit-resp.net_used);
+    let need_trx= ((energy_need as i64)-energy_free)* 280;
+    println!("trx need:{}",need_trx);
+}
+
+async fn get_trc10_fee_test(){
+    let mut client =  get_client().await;
+    let from=hex::decode( ADDR_HEX_1).expect("decode error");
+    let to=hex::decode( ADDR_HEX_2).expect("decode error");
+    let amount =5000000u128; // 2个
+    let asset_id="1000016";
+
+    let secret_obj=private::Private::from_hex (PRIVATE_KEY_1).expect("decode error");
+    let mut contract_addr_bytes=   "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj".from_base58().expect("base 58 decode error");
+    println!("contract addr:{}",hex::encode(&contract_addr_bytes[..contract_addr_bytes.len()-4].to_vec()));
+    //contract_addr_bytes=contract_addr_bytes[1..].to_vec(); // 地址第一个字节是tron前缀，所以需要去掉
+
+    let contract_type=tron_grpc::transaction::contract::ContractType::TransferAssetContract;
+    let transfer_tx = tron_grpc::TransferAssetContract{
+        asset_name: asset_id.as_bytes().to_vec(),
+        owner_address: from,
+        to_address: to,
+        amount:amount as i64
+    };
+
+    // energy ->通过接口获取
+    // bandwidth -->是一个常量
+    // trx
+    //let energy_need= get_trc20_energy_need(&mut client,ADDR_HEX_1,ADDR_HEX_2,amount.as_u128()).await;
+    let bindwidth_need=get_tx_bandwidth_need(&mut client,transfer_tx,contract_type).await;
+    println!("bind width need:{}",bindwidth_need);
+}
+
+async fn get_trx_fee_test(){
+    let mut client =  get_client().await;
+    let from=hex::decode( ADDR_HEX_1).expect("decode error");
+    let to=hex::decode( ADDR_HEX_2).expect("decode error");
+    let amount =5000000u128; // 2个
+    let asset_id="1000016";
+
+    let secret_obj=private::Private::from_hex (PRIVATE_KEY_1).expect("decode error");
+    let mut contract_addr_bytes=   "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj".from_base58().expect("base 58 decode error");
+    println!("contract addr:{}",hex::encode(&contract_addr_bytes[..contract_addr_bytes.len()-4].to_vec()));
+    //contract_addr_bytes=contract_addr_bytes[1..].to_vec(); // 地址第一个字节是tron前缀，所以需要去掉
+
+    let contract_type=tron_grpc::transaction::contract::ContractType::TransferContract;
+    let transfer_tx = tron_grpc::TransferContract{
+        owner_address: from,
+        to_address: to,
+        amount:amount as i64
+    };
+
+    // energy ->通过接口获取
+    // bandwidth -->是一个常量
+    // trx
+    //let energy_need= get_trc20_energy_need(&mut client,ADDR_HEX_1,ADDR_HEX_2,amount.as_u128()).await;
+    let bindwidth_need=get_tx_bandwidth_need(&mut client,transfer_tx,contract_type).await;
+    println!("bind width need:{}",bindwidth_need);
+}
+
+/// 计算参考文档:
+/// https://cn.developers.tron.network/docs/resource-model
+/// https://github.com/tronprotocol/java-tron/issues/2982
+/// https://developers.tron.network/docs/faq#5-how-to-calculate-the-bandwidth-and-energy-consumed-when-calling-the-contract
+async fn get_trc20_energy_need(client:&mut WalletClient<Channel>,from:&str,to:&str,amount:u128)->u64{
+    let from=hex::decode( from).expect("decode error");
+    let to=hex::decode( to).expect("decode error");
+    let amount:ethers::types::U256 =U256::from( amount); // 2个
+    let mut contract_addr_bytes=   "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj".from_base58().expect("base 58 decode error");
+
+    let fn_obj= ABI.function("transfer").expect("get fn error");
+    let param_data= ethers::contract::encode_function_data(fn_obj,(Address::from_slice(&to[1..]),amount)).expect("encode fn param error");
+    println!("param data:{}",hex::encode( param_data.as_ref()));
+
+    let transfer_tx = tron_grpc::TriggerSmartContract{
+        owner_address: from,
+        contract_address: contract_addr_bytes[..contract_addr_bytes.len()-4].to_vec(),
+        call_value: 0,
+        data: param_data.to_vec(),
+        call_token_value: 0,
+        token_id: 0
+    };
+    // energy ->通过接口获取
+    // bandwidth -->是一个常量
+    // trx
+    let result= client.trigger_constant_contract(transfer_tx).await.expect("broadcast error");
+    let result=result.into_inner();
+    result.energy_used as u64
+}
+
+/// 计算参考文档
+/// https://developers.tron.network/docs/bandwith
+/// https://github.com/tronprotocol/java-tron/issues/2128
+/// https://cn.developers.tron.network/docs/resource-model
+async fn get_tx_bandwidth_need<TInput:Message>(client:&mut WalletClient<Channel>,contract:TInput,contract_type:ContractType)->u64{
+    let mut raw_bytes:Vec<u8>=Vec::new();
+    contract.encode(&mut raw_bytes);
+    let tx_any= prost_types::Any{
+        type_url: format!("type.googleapis.com/protocol.{:?}", &contract_type),
+        value: raw_bytes
+    };
+
+    let contract=tron_grpc::transaction::Contract{
+        r#type: contract_type.clone() as i32 ,
+        parameter: Some(tx_any),
+        provider: vec![],
+        contract_name: vec![],
+        permission_id: 0
+    };
+
+    let block_num=0i64;
+
+    let now=chrono::Utc::now().timestamp_millis();
+    let mut raw= tron_grpc::transaction::Raw{
+        ref_block_bytes: vec![
+            ((block_num & 0xff00) >> 8) as u8,
+            (block_num & 0xff) as u8,
+        ],
+        ref_block_num:block_num ,
+        ref_block_hash: vec![0u8;8].to_owned(),
+        expiration: now+61*1000, // 61秒过期
+        auths: vec![],
+        data: vec![],
+        contract: vec![contract],
+        scripts: vec![],
+        timestamp:now,
+        fee_limit: 100000
+    };
+    let mut raw_bytes:Vec<u8>=Vec::new();
+    raw.encode(&mut raw_bytes);
+    println!("raw bytes len:{}",raw_bytes.len());
+
+    let req=tron_grpc::Transaction{
+        raw_data: Some(raw),
+        signature: vec![vec![0u8;65]],
+        ret: vec![]
+    };
+
+    let mut raw_bytes:Vec<u8>=Vec::new();
+    req.encode(&mut raw_bytes);
+    println!("req bytes len:{}",raw_bytes.len());
+
+    const REQUEST_EXTRA_BYTES:u64=70;
+    REQUEST_EXTRA_BYTES+raw_bytes.len() as u64
 }
